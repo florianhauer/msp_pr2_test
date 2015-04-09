248195204b5ac0dc6ba5c6767e9f16e8c087f8d4
Florian Hauer <flo@gatech.edu>
2015-04-09 17:53:47 -0400

    2arms video

--- src/2arms.cpp
+++ src/2arms.cpp
@@ -107,10 +107,11 @@ template <unsigned int DIM> bool addObstacles(Key<DIM> k, int depth, int size, T
 }
 
 void addObstaclesToPlanningScene(){
+	//add book in right hand
 	moveit_msgs::AttachedCollisionObject attached_object;
 	attached_object.link_name = "r_wrist_roll_link";
 	attached_object.object.header.frame_id = "r_wrist_roll_link";
-	attached_object.object.id = "box";
+	attached_object.object.id = "book_r";
 	geometry_msgs::Pose pose;
 	pose.position.x=0.2;
 	pose.orientation.w = 1.0;
@@ -120,6 +121,16 @@ void addObstaclesToPlanningScene(){
 	primitive.dimensions[0] = 0.15;
 	primitive.dimensions[1] = 0.05;
 	primitive.dimensions[2] = 0.25;  
+	attached_object.object.primitives.push_back(primitive);
+	attached_object.object.primitive_poses.push_back(pose);
+	attached_object.object.operation = attached_object.object.ADD;
+	//ADD book
+	scene->processAttachedCollisionObjectMsg(attached_object);
+	// add book in left hand too
+	attached_object=moveit_msgs::AttachedCollisionObject();
+	attached_object.link_name = "l_wrist_roll_link";
+	attached_object.object.header.frame_id = "l_wrist_roll_link";
+	attached_object.object.id = "book_l";
 	attached_object.object.primitives.push_back(primitive);
 	attached_object.object.primitive_poses.push_back(pose);
 	attached_object.object.operation = attached_object.object.ADD;
@@ -185,8 +196,8 @@ int main(int argc, char **argv)
 	//set base position
 	group=kinematic_model->getJointModelGroup("base");
 	current_state->copyJointGroupPositions(group,group_variable_values);
-	group_variable_values[0] = 0.00; //set x to be close from the bookshelves
-	group_variable_values[1] = 0.00; //set y
+	group_variable_values[0] = 0.03; //set x to be close from the bookshelves
+	group_variable_values[1] = -0.03; //set y
 	setState(*current_state,group_variable_values);
 	//publishState(*current_state);
 	static tf::TransformBroadcaster br;
@@ -212,28 +223,28 @@ int main(int argc, char **argv)
 	//set current group to right arm for planning
 	group=kinematic_model->getJointModelGroup("right_arm");
 	current_state->copyJointGroupPositions(group,group_variable_values);
-	std::vector<float> startvec(5);
-	startvec[0]=0.001f;
-	startvec[1]=-0.35f;
-	startvec[2]=-3.14f;
-	startvec[3]=-0.21f;
-	startvec[4]=-3.1f;
-	startvec[5]=-0.11f;
+	std::vector<float> startvecr(5);
+	startvecr[0]=0.001f;
+	startvecr[1]=1.201f;
+	startvecr[2]=-0.01f;
+	startvecr[3]=-0.8499f;
+	startvecr[4]=-3.1f;
+	startvecr[5]=-0.11f;
 	for(int i=0;i<group_variable_values.size();++i){
-		group_variable_values[i] = startvec[i];
+		group_variable_values[i] = startvecr[i];
 	}
-	group_variable_values[0]=0.001f;
+	/*group_variable_values[0]=0.001f;
 	group_variable_values[1]=-0.35f;
 	group_variable_values[2]=-3.14f;
 	group_variable_values[3]=-0.21f;
 	group_variable_values[4]=-3.1f;
-	group_variable_values[5]=-0.11f;
+	group_variable_values[5]=-0.11f;*/
 	setState(*current_state,group_variable_values);
 	publishState(*current_state);
 	sleeper5.sleep();
 	sleeper5.sleep();
 	collision_detection::CollisionRobotPtr colRob = scene->getCollisionRobotNonConst ();
-	colRob->setPadding(0.04);
+	colRob->setPadding(0.05);
 	scene->propogateRobotPadding ();
 	moveit::core::JointBoundsVector bvec=group->getActiveJointModelsBounds();
 	//*
@@ -246,11 +257,11 @@ int main(int argc, char **argv)
 	//Create Tree
 	Tree<AD>* t=new Tree<AD>();
 	//Set Search Space Bounds
-	State<AD> minState;
-	State<AD> maxState;
+	State<AD> minStater;
+	State<AD> maxStater;
 	for(int i=0;i<AD;++i){
-		minState[i]=(*(bvec[i]))[0].min_position_;
-		maxState[i]=(*(bvec[i]))[0].max_position_;
+		minStater[i]=(*(bvec[i]))[0].min_position_;
+		maxStater[i]=(*(bvec[i]))[0].max_position_;
 	}
 	/*State<AD> minState(-1.5f);
 	State<AD> maxState(1.5f);
@@ -258,23 +269,24 @@ int main(int argc, char **argv)
 	minState[3]=-2.25;
 	maxState[2]=0.75;
 	maxState[3]=0.25;*/
-	t->setStateBounds(minState,maxState);
+	t->setStateBounds(minStater,maxStater);
 	//Set Tree Max Depth
 	int depth=5;
 	t->setMaxDepth(depth);
 	//Start and goal
-	State<AD> start(0.001f);
+	State<AD> startr(0.001f);
 	for(int i=0;i<AD;++i){
-		start[i]=startvec[i];
+		startr[i]=startvecr[i];
 	}
-	State<AD> goal(0.001f);
-	goal[1]=0.31f;
-	goal[3]=-0.31f;
+	State<AD> goalr(0.001f);
+	goalr[1]=-0.35f;
+	goalr[2]=-3.14f;
+	goalr[3]=-0.21f;
 	//Test if start and goal would be obstacle free before creating the entire map
 	Key<AD> k;
-	if(!(t->getKey(start,k) && !isObstacle(t->getState(k)))){
+	if(!(t->getKey(startr,k) && !isObstacle(t->getState(k)))){
 		std::cout << "start not in free space" << std::endl;
-		std::cout << "desired start : " << start << std::endl;
+		std::cout << "desired start : " << startr << std::endl;
 		std::cout << "actual start : " << t->getState(k) << " with key " << k << std::endl;
 		std::cout << "is obstacle : " << isObstacle(t->getState(k)) << std::endl;
 		for(int i=0;i<group_variable_values.size();++i){
@@ -284,9 +296,9 @@ int main(int argc, char **argv)
 		publishState(*current_state);
 		exit(1);
 	}
-	if(!(t->getKey(goal,k) && !isObstacle(t->getState(k)))){
+	if(!(t->getKey(goalr,k) && !isObstacle(t->getState(k)))){
 		std::cout << "goal not in free space" << std::endl;
-		std::cout << "desired goal : " << goal << std::endl;
+		std::cout << "desired goal : " << goalr << std::endl;
 		std::cout << "actual goal : " << t->getState(k) << " with key " << k << std::endl;
 		for(int i=0;i<group_variable_values.size();++i){
 			group_variable_values[i] = t->getState(k)[i];
@@ -298,7 +310,6 @@ int main(int argc, char **argv)
 	//Depth First Obstacle Creation
 	std::cout << "Obstacle creation " << std::setw(10) << 0.0 << "\% done.";
 	timerStart=time(NULL);
-	//addObstacles(t->getRootState(),0,1.0f,t);
 	addObstacles(t->getRootKey(),0,t->getRootKey()[0],t);
 	std::cout << std::endl;
 	time_t timerNow=time(NULL);	
@@ -316,11 +327,13 @@ int main(int argc, char **argv)
 //	std::cout<<*(t->getRoot())<<std::endl;
 //	std::cout.width(prev);
 
+	//variables to hold results
+	std::deque<State<AD>> solr,soll;
 	//Create algo
 	std::cout << "create algo" << std::endl;
 	MSP<AD> algo(t);
 	//Set algo parameters
-	bool initAlgo=algo.init(start,goal);
+	bool initAlgo=algo.init(startr,goalr);
 	std::cout << "init algo " <<initAlgo << std::endl;
 	algo.setAlpha(2*sqrt(AD));
 	algo.setSpeedUp(true);
@@ -348,37 +361,230 @@ int main(int argc, char **argv)
 		std::cout << "smoothed solution" <<std::endl;
 		sol=algo.getSmoothedPath();
 		std::cout << "Path length: " << sol.size() << std::endl;
-		//Visualize results
-		ros::Duration sleeper1(0.01);
-		State<AD> prev(start);
-		double pas=0.005;
-		while(ros::ok()){
-			for(std::deque<State<AD>>::iterator it=sol.begin(),end=sol.end();it!=end;++it){
-				while((prev-*it).norm()>pas){
-					prev=prev+(*it-prev)*(pas/(*it-prev).norm());
-					for(int i=0;i<AD;++i){
-						group_variable_values[i]=prev[i];
-					}
-					setState(*current_state,group_variable_values);
-					publishState(*current_state);
-					sleeper1.sleep();
-				}
-			}
-			sleeper5.sleep();
-			prev=start;
-			for(int i=0;i<AD;++i){
-				group_variable_values[i]=prev[i];
-			}
-			setState(*current_state,group_variable_values);
-			publishState(*current_state);
-			sleeper5.sleep();
-		}
+		solr=sol;
 	}else{
 		std::cout << "no solution found or init failed" <<std::endl;
 	}
 
 	delete t;
 
+	//Plan for the second arm
+	std::cout << "NOW PLANINNG FOR SECOND ARM" << std::endl;
+	for(int i=0;i<group_variable_values.size();++i){
+		group_variable_values[i] = startvecr[i];
+	}
+	setState(*current_state,group_variable_values);
+	publishState(*current_state);
+
+	group=kinematic_model->getJointModelGroup("left_arm");
+	current_state->copyJointGroupPositions(group,group_variable_values);
+
+	std::vector<float> startvecl(5);
+	startvecl[0]=-0.001f;
+	startvecl[1]=-0.35f;
+	startvecl[2]=3.14f;
+	startvecl[3]=-0.21f;
+	startvecl[4]=3.1f;
+	startvecl[5]=-0.11f;
+	for(int i=0;i<group_variable_values.size();++i){
+		group_variable_values[i] = startvecl[i];
+	}
+	setState(*current_state,group_variable_values);
+	publishState(*current_state);
+
+	bvec=group->getActiveJointModelsBounds();
+	//*
+	for(auto v: bvec){
+		for(auto v2: (*v)){
+			std::cout << v2.min_position_ << " , " << v2.max_position_ << std::endl;
+		}
+		std::cout <<std::endl;
+	}//*/
+	//Set Search Space Bounds
+	State<AD> minStatel;
+	State<AD> maxStatel;
+	for(int i=0;i<AD;++i){
+		minStatel[i]=(*(bvec[i]))[0].min_position_;
+		maxStatel[i]=(*(bvec[i]))[0].max_position_;
+	}
+	
+	//Create Tree
+	t=new Tree<AD>();
+	//Set Search Space Bounds
+	t->setStateBounds(minStatel,maxStatel);
+	t->setMaxDepth(depth);
+	//Start and goal
+	State<AD> startl(0.001f);
+	for(int i=0;i<AD;++i){
+		startl[i]=startvecl[i];
+	}
+	State<AD> goall(-0.001f);
+	goall[1]=0.31f;
+	goall[3]=-0.31f;
+	//Test if start and goal would be obstacle free before creating the entire map
+	if(!(t->getKey(startl,k) && !isObstacle(t->getState(k)))){
+		std::cout << "start not in free space" << std::endl;
+		std::cout << "desired start : " << startl << std::endl;
+		std::cout << "actual start : " << t->getState(k) << " with key " << k << std::endl;
+		std::cout << "is obstacle : " << isObstacle(t->getState(k)) << std::endl;
+		for(int i=0;i<group_variable_values.size();++i){
+			group_variable_values[i] = t->getState(k)[i];
+		}
+		setState(*current_state,group_variable_values);
+		publishState(*current_state);
+		exit(1);
+	}
+	if(!(t->getKey(goall,k) && !isObstacle(t->getState(k)))){
+		std::cout << "goal not in free space" << std::endl;
+		std::cout << "desired goal : " << goall << std::endl;
+		std::cout << "actual goal : " << t->getState(k) << " with key " << k << std::endl;
+		for(int i=0;i<group_variable_values.size();++i){
+			group_variable_values[i] = t->getState(k)[i];
+		}
+		setState(*current_state,group_variable_values);
+		publishState(*current_state);
+		exit(1);
+	}
+	//Depth First Obstacle Creation
+	std::cout << "Obstacle creation " << std::setw(10) << 0.0 << "\% done.";
+	timerStart=time(NULL);
+	Ocount=0;
+	//addObstacles(t->getRootState(),0,1.0f,t);
+	addObstacles(t->getRootKey(),0,t->getRootKey()[0],t);
+	std::cout << std::endl;
+	timerNow=time(NULL);	
+	seconds = (int)difftime(timerNow,timerStart);
+	hours = seconds/3600;
+	minutes= (seconds/60)%60;
+	seconds=seconds%60;
+	std::cout << "Obstacles created in " 	<< std::setw(4) << hours << ":" 
+						<< std::setw(2) << minutes << ":" 
+						<< std::setw(2) << seconds 
+						<< std::endl;
+	//	//print tree to check
+//	std::streamsize prev=std::cout.width(0);
+//	std::cout.flags(std::ios_base::right);
+//	std::cout<<*(t->getRoot())<<std::endl;
+//	std::cout.width(prev);
+
+	//variables to hold results
+	//Create algo
+	std::cout << "create algo" << std::endl;
+	algo=MSP<AD>(t);
+	//Set algo parameters
+	initAlgo=algo.init(startl,goall);
+	std::cout << "init algo " <<initAlgo << std::endl;
+	algo.setAlpha(2*sqrt(AD));
+	algo.setSpeedUp(true);
+	//Run algo
+	timerStart=time(NULL);
+	if(initAlgo && algo.run()){
+		timerNow=time(NULL);	
+		int seconds = (int)difftime(timerNow,timerStart);
+		int hours = seconds/3600;
+		int minutes= (seconds/60)%60;
+		seconds=seconds%60;
+		std::cout << "Solution found in " 	<< std::setw(4) << hours << ":" 
+						<< std::setw(2) << minutes << ":" 
+						<< std::setw(2) << seconds 
+						<< std::endl;
+		std::cout << "raw solution" <<std::endl;
+		std::deque<State<AD>> sol=algo.getPath();
+		std::cout << "Path length: " << sol.size() << std::endl;
+		std::cout << "Path cost: " << algo.getPathCost() << std::endl;
+		//std::cout << "Path :" << std::endl;
+		//for(std::deque<State<AD>>::iterator it=sol.begin(),end=sol.end();it!=end;++it){
+		//	std::cout << (*it) << " -- ";
+		//}
+		//std::cout << std::endl;
+		std::cout << "smoothed solution" <<std::endl;
+		sol=algo.getSmoothedPath();
+		std::cout << "Path length: " << sol.size() << std::endl;
+		soll=sol;
+	}else{
+		std::cout << "no solution found or init failed" <<std::endl;
+	}
+
+	delete t;
+	
+	//Visualize results
+	ros::Duration sleeper1(0.01);
+	State<AD> prevl(startl);
+	State<AD> prevr(startr);
+	int indexl=0;
+	int indexr=0;
+	double pas=0.005;
+	const moveit::core::JointModelGroup* groupl=kinematic_model->getJointModelGroup("left_arm");
+	const moveit::core::JointModelGroup* groupr=kinematic_model->getJointModelGroup("right_arm");
+	std::vector<double> group_variable_valuesl;
+	std::vector<double> group_variable_valuesr;
+	current_state->copyJointGroupPositions(groupr,group_variable_valuesr);
+	current_state->copyJointGroupPositions(groupl,group_variable_valuesl);
+	while(ros::ok()){
+		bool donel=false;
+		bool doner=false;
+		/*for(std::deque<State<AD>>::iterator it=solr.begin(),end=solr.end();it!=end;++it){
+			while((prev-*it).norm()>pas){
+				prev=prev+(*it-prev)*(pas/(*it-prev).norm());
+				for(int i=0;i<AD;++i){
+					group_variable_values[i]=prev[i];
+				}
+				setState(*current_state,group_variable_values);
+				publishState(*current_state);
+				sleeper1.sleep();
+			}
+		}*/
+		while(!donel || !doner){
+			if(!donel){
+				if((prevl-soll[indexl]).norm()>pas){
+					prevl=prevl+(soll[indexl]-prevl)*(pas/(soll[indexl]-prevl).norm());
+					for(int i=0;i<AD;++i){
+						group_variable_valuesl[i]=prevl[i];
+					}
+					current_state->setJointGroupPositions(groupl,group_variable_valuesl);
+				}else{
+					if(indexl<soll.size()-1){
+						indexl++;
+					}else{
+						donel=true;
+					}
+				}
+			}
+			if(!doner){
+				if((prevr-solr[indexr]).norm()>pas){
+					prevr=prevr+(solr[indexr]-prevr)*(pas/(solr[indexr]-prevr).norm());
+					for(int i=0;i<AD;++i){
+						group_variable_valuesr[i]=prevr[i];
+					}
+					current_state->setJointGroupPositions(groupr,group_variable_valuesr);
+				}else{
+					if(indexr<solr.size()-1){
+						indexr++;
+					}else{
+						doner=true;
+					}
+				}
+			}
+			publishState(*current_state);
+			sleeper1.sleep();
+			
+		}
+		sleeper5.sleep();
+		prevl=startl;
+		for(int i=0;i<AD;++i){
+			group_variable_valuesl[i]=prevl[i];
+		}
+		prevr=startr;
+		for(int i=0;i<AD;++i){
+			group_variable_valuesr[i]=prevr[i];
+		}
+		current_state->setJointGroupPositions(groupl,group_variable_valuesl);
+		current_state->setJointGroupPositions(groupr,group_variable_valuesr);
+		indexl=0;
+		indexr=0;
+		publishState(*current_state);
+		sleeper5.sleep();
+	}
 	std::cout << "no crash " << std::endl;
 	ros::shutdown();
 	return 0;
